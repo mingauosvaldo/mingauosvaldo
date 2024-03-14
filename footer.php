48b275e6a3956e207fe5a4c39d6af1c645bf6dcd
mingauosvaldo <163464475+mingauosvaldo@users.noreply.github.com>
2024-03-14 17:45:22 -0300

    Avaliação

---
+++ footer.php
@@ -0,0 +1,8 @@
+</div>
+
+<!-- Container que abriga o rodapé da página -->
+<div class="container-fluid text-center bg-dark text-light fixed-bottom">
+    <p>Sistema Web desenvolvido por Maria</p>
+
+</body>
+</html>
