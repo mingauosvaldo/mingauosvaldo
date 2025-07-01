78af09854cb4c632b0860f19d718afd3c131a49e
mingauosvaldo <163464475+mingauosvaldo@users.noreply.github.com>
2025-07-01 14:49:20 -0300

    Delete footer.php

--- footer.php
+++
@@ -1,8 +0,0 @@
-</div>
-
-<!-- Container que abriga o rodapé da página -->
-<div class="container-fluid text-center bg-dark text-light fixed-bottom">
-    <p>Sistema Web desenvolvido por Maria</p>
-
-</body>
-</html>
